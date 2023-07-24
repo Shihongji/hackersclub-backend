41bcf775f69fd0c7ac8e25b914c5af7c6b3d141c
hongji <shihongji21@gmail.com>
2023-07-24 14:07:42 -0700

    sep upload middleware

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,6 @@
 import auth from "../middleware/auth.js";
 import express from "express";
-import multer from "multer";
-import path from "path";
-
-// This will configure multer to store incoming files in a directory named "uploads" in your server.
-// Each file will be given a unique filename.
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); // Append extension
-  },
-});
-
-const upload = multer({ storage });
+import upload from "../middleware/uploadImage.js";
 
 const router = express.Router();
 import {
